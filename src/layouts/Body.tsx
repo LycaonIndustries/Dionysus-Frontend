bac64e72e893e6321052b07b15965c879a5718e0
Rishabh Anand <thefenrislycaon@gmail.com>
2024-07-25 12:26:04 +0530

    change bg color

--- src/layouts/Body.tsx
+++ src/layouts/Body.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Header from "./Header";
 
 // Define the Body component as a functional component with React.FC type
 // Accepts children of type React.ReactNode as props
 const Body: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   // Render the main content area with gradient background and responsive padding
   return (
-    <main className="bg-slate-950 h-full min-h-screen w-full min-w-full pt-24">
+    <main className="h-full pt-24 min-h-screen w-full min-w-full bg-gradient-to-b from-background-950 from-10% via-primary-900 to-90% to-background-950">
+      {/* Render the Header component */}
+      <Header />
       {/* Render the children passed to the Body component */}
       {children}
     </main>
